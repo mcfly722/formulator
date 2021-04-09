f7bb513d19db45a015d1cf71c90c4030bfe0a942
Anonymous <7221798@gmail.com>
2021-04-09 22:46:11 +0300

    brackets to strings

--- Catalan.go
+++ Catalan.go
@@ -2,18 +2,32 @@ package main
 
 import "fmt"
 
-// Node node of binary tree
-type Node struct {
+// Bracket node of binary tree
+type BracketStep struct {
 	x int
 	y int
 }
 
-func printBrackets(tail []Node) {
-	fmt.Println(fmt.Sprintf("%v", tail))
+func printBrackets(tail []BracketStep) {
+	fmt.Println(BracketsStepsToString(tail))
+}
+
+// BracketsStepsToString represent bracket steps as string
+func BracketsStepsToString(tail []BracketStep) string {
+	output:=""
+	for _,step := range tail {
+		for i:=0;i<step.x;i++{
+			output+=fmt.Sprintf("(");
+		}
+		for i:=0;i<step.y;i++{
+			output+=fmt.Sprintf(")");
+		}
+	}
+	return output
 }
 
 // NextBracket recursively generates bracket ledder (each element is one ledder step with width and height)
-func NextBracket(tail []Node, sizeX int, sizeY int, output func([]Node)) {
+func NextBracket(tail []BracketStep, sizeX int, sizeY int, output func([]BracketStep)) {
 	//fmt.Println(fmt.Sprintf("next bracket"))
 
 	if sizeX == 0 && sizeY == 0 {
@@ -21,11 +35,11 @@ func NextBracket(tail []Node, sizeX int, sizeY int, output func([]Node)) {
 		return
 	}
 
-	for i := sizeX; i >= 1; i-- {
+	for i := 1; i <= sizeX; i++ {
 		for j := 1; j <= sizeY-(sizeX-i); j++ {
 
 			//fmt.Println(fmt.Sprintf("%v,%v, %v,%v", i, j, size_x, size_y))
-			nextTail := append(tail, Node{x: i, y: j})
+			nextTail := append(tail, BracketStep{x: i, y: j})
 
 			NextBracket(nextTail, sizeX-i, sizeY-j, output)
 		}
@@ -33,5 +47,5 @@ func NextBracket(tail []Node, sizeX int, sizeY int, output func([]Node)) {
 }
 
 func main() {
-	NextBracket([]Node{}, 4, 4, printBrackets)
+	NextBracket([]BracketStep{}, 4, 4, printBrackets)
 }
