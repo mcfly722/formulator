1d9e4808ac6a4cfeeea270110807fdc9008f74c3
Anonymous <7221798@gmail.com>
2021-04-11 23:15:50 +0300

    Bracket -> Binary tree algorithm works

--- Catalan.go
+++ Catalan.go
@@ -2,25 +2,81 @@ package main
 
 import "fmt"
 
-// Bracket node of binary tree
+// BracketStep node of binary tree
 type BracketStep struct {
 	x int
 	y int
 }
 
+// Node of binary tree
+type Node struct {
+	Parent *Node
+	Childs []*Node
+}
+
+func newNode(parent *Node) *Node {
+	node := &Node{
+		Parent: parent,
+		Childs: []*Node{}}
+	return node
+}
+
+// ToString converts binary tree with root to string representation
+func (node *Node) ToString() string {
+	if len(node.Childs) == 0 {
+		return "*"
+	}
+
+	output := ""
+	for _, n := range node.Childs {
+		output += n.ToString()
+	}
+	return fmt.Sprintf("(%v)", output)
+}
+
 func printBrackets(tail []BracketStep) {
-	fmt.Println(BracketsStepsToString(tail))
+	treeRoot := BracketsStepsToBinaryTree(tail)
+
+	fmt.Println(fmt.Sprintf("%v -> %v", BracketsStepsToString(tail), treeRoot.ToString()))
+
+}
+
+// BracketsStepsToBinaryTree converts brackets steps to binary tree
+func BracketsStepsToBinaryTree(tail []BracketStep) *Node {
+	root := newNode(nil)
+	openedBracketSequence := []*Node{root}
+
+	for _, step := range tail {
+
+		for i := 0; i < step.x; i++ {
+			current := openedBracketSequence[len(openedBracketSequence)-1]
+			node := newNode(current)
+			current.Childs = append(current.Childs, node)
+			openedBracketSequence = append(openedBracketSequence, node)
+		}
+
+		for i := 0; i < step.y; i++ {
+			validParent := openedBracketSequence[len(openedBracketSequence)-1].Parent
+			for ; len(validParent.Childs) == 2; validParent = validParent.Parent {
+			} // search last parent with one operand
+
+			node := newNode(validParent)
+			validParent.Childs = append(validParent.Childs, node)
+			openedBracketSequence = append(openedBracketSequence, node)
+		}
+	}
+	return root
 }
 
 // BracketsStepsToString represent bracket steps as string
 func BracketsStepsToString(tail []BracketStep) string {
-	output:=""
-	for _,step := range tail {
-		for i:=0;i<step.x;i++{
-			output+=fmt.Sprintf("(");
+	output := ""
+	for _, step := range tail {
+		for i := 0; i < step.x; i++ {
+			output += fmt.Sprintf("(")
 		}
-		for i:=0;i<step.y;i++{
-			output+=fmt.Sprintf(")");
+		for i := 0; i < step.y; i++ {
+			output += fmt.Sprintf(")")
 		}
 	}
 	return output
@@ -47,5 +103,5 @@ func NextBracket(tail []BracketStep, sizeX int, sizeY int, output func([]Bracket
 }
 
 func main() {
-	NextBracket([]BracketStep{}, 4, 4, printBrackets)
+	NextBracket([]BracketStep{}, 6, 6, printBrackets)
 }
