a0357b2d836aa3d8765dc0e78ccbd65edd901c6d
Anonymous <7221798@gmail.com>
2021-04-16 03:29:31 +0300

    next function without errors and full tests covering

--- Catalan.go
+++ Catalan.go
@@ -8,145 +8,16 @@ type BracketStep struct {
 	y int
 }
 
-// Node of binary tree
-type Node struct {
-	Parent *Node
-	Childs []*Node
-}
-
-func newNode(parent *Node) *Node {
-	node := &Node{
-		Parent: parent,
-		Childs: []*Node{}}
-	return node
-}
-
-// ToString converts binary tree with root to string representation
-func (node *Node) ToString() string {
-	if len(node.Childs) == 0 {
-		return "*"
+func stringToBracketsSteps(input string) ([]BracketStep, int, error) {
+	if input == "" {
+		return nil, -1, fmt.Errorf("could not parse empty string, use () for first sequence instead")
 	}
 
-	output := ""
-	for _, n := range node.Childs {
-		output += n.ToString()
-	}
-	return fmt.Sprintf("(%v)", output)
-}
-
-func printBrackets(tail []BracketStep) {
-	treeRoot := BracketsStepsToBinaryTree(tail)
-
-	root, err := StringToBinaryTree(treeRoot.ToString(), nil)
-
-	if err != nil {
-		fmt.Println(fmt.Sprintf("%v -> %v error:%v", BracketsStepsToString(tail), treeRoot.ToString(), err))
-	}
-
-	nextBrackets, err := getNextBracketsTree(BracketsStepsToString(tail))
-	if err != nil {
-		fmt.Println(fmt.Sprintf("%v error:%v", BracketsStepsToString(tail), err))
-	}
-
-	fmt.Println(fmt.Sprintf("%v -> %v -> %v equal=%v %v", BracketsStepsToString(tail), treeRoot.ToString(), root.ToString(), treeRoot.ToString() == root.ToString(), nextBrackets))
-}
-
-// BracketsStepsToBinaryTree converts brackets steps to binary tree
-func BracketsStepsToBinaryTree(tail []BracketStep) *Node {
-	root := newNode(nil)
-	openedBracketSequence := []*Node{root}
-
-	for _, step := range tail {
-
-		for i := 0; i < step.x; i++ {
-			current := openedBracketSequence[len(openedBracketSequence)-1]
-			node := newNode(current)
-			current.Childs = append(current.Childs, node)
-			openedBracketSequence = append(openedBracketSequence, node)
-		}
-
-		for i := 0; i < step.y; i++ {
-			validParent := openedBracketSequence[len(openedBracketSequence)-1].Parent
-			for ; len(validParent.Childs) == 2; validParent = validParent.Parent {
-			} // search last parent with one operand
-
-			node := newNode(validParent)
-			validParent.Childs = append(validParent.Childs, node)
-			openedBracketSequence = append(openedBracketSequence, node)
-		}
-	}
-	return root
-}
-
-// StringToBinaryTree deserialize from string to binary tree
-func StringToBinaryTree(input string, parent *Node) (*Node, error) {
-	node := newNode(parent)
-
-	if input == "*" {
-		return node, nil
-	}
-
-	if input[0] == '(' && input[len(input)-1] == ')' {
-		pos := 1
-		bracketsCounter := 1
-		for ; bracketsCounter > 0 && pos < len(input); pos++ {
-			if input[pos] == '(' {
-				bracketsCounter++
-			}
-			if input[pos] == ')' {
-				bracketsCounter--
-			}
-		}
-		if bracketsCounter > 0 {
-			return nil, fmt.Errorf("Opened brackets are not equal to closed in '%v' diff=%v", input, bracketsCounter)
-		}
-
-		if pos == len(input) {
-			return StringToBinaryTree(input[1:len(input)-1], node)
-		}
-
-		childNode1, err := StringToBinaryTree(input[0:pos], node)
-		if err != nil {
-			return nil, err
-		}
-
-		childNode2, err := StringToBinaryTree(input[pos:len(input)], node)
-		if err != nil {
-			return nil, err
-		}
-
-		node.Childs = append(node.Childs, childNode1)
-		node.Childs = append(node.Childs, childNode2)
-		return node, nil
-	}
-
-	if input[0] == '*' {
-		node.Childs = append(node.Childs, newNode(parent))
-		childNode, err := StringToBinaryTree(input[1:len(input)], node)
-		if err != nil {
-			return nil, err
-		}
-		node.Childs = append(node.Childs, childNode)
-		return node, nil
-	}
-
-	if input[0] == '(' && input[len(input)-1] == '*' {
-		childNode, err := StringToBinaryTree(input[1:len(input)-2], node)
-		if err != nil {
-			return nil, err
-		}
-		node.Childs = append(node.Childs, childNode)
-		node.Childs = append(node.Childs, newNode(parent))
-		return node, nil
-	}
-
-	return nil, fmt.Errorf("Could not parse '%v'", input)
-}
-
-// StringToBracketsSteps converts brackets to steps
-func StringToBracketsSteps(input string) ([]BracketStep, int, error) {
 	steps := []BracketStep{}
 	i, x, y := 0, 0, 0
+
+	counter := 0
+
 	for i < len(input) {
 		step := BracketStep{
 			x: 0, y: 0,
@@ -155,15 +26,20 @@ func StringToBracketsSteps(input string) ([]BracketStep, int, error) {
 		for ; i < len(input) && input[i] == '('; i++ {
 			step.x++
 			x++
+			counter++
 		}
 
 		for ; i < len(input) && input[i] == ')'; i++ {
 			step.y++
 			y++
+			counter--
+			if counter < 0 {
+				return nil, -1, fmt.Errorf("%v<- incorrect brackets balance, could not close not opened bracket", input[0:i+1])
+			}
 		}
 
 		if i < len(input) && input[i] != '(' && input[i] != ')' {
-			return nil, -1, fmt.Errorf("%v<- unknown symbol", input[0:i])
+			return nil, -1, fmt.Errorf("%v<- unknown symbol", input[0:i+1])
 		}
 		steps = append(steps, step)
 	}
@@ -186,14 +62,18 @@ func getNextStepsTail(srcTail []BracketStep, dstTail []BracketStep, sizeX int, s
 	newDstTail := []BracketStep{}
 
 	startX := 1
-	startY := 1
 
 	if !previousSolutionAlreadyReached && len(srcTail) > 0 {
 		startX = srcTail[0].x
-		startY = srcTail[0].y
 	}
 
 	for i := startX; i <= sizeX; i++ {
+
+		startY := 1
+		if !previousSolutionAlreadyReached && len(srcTail) > 0 {
+			startY = srcTail[0].y
+		}
+
 		for j := startY; j <= sizeY-(sizeX-i); j++ {
 
 			newDstTail = append(dstTail, BracketStep{x: i, y: j})
@@ -213,11 +93,14 @@ func getNextStepsTail(srcTail []BracketStep, dstTail []BracketStep, sizeX int, s
 
 		}
 	}
+
 	return []BracketStep{}, previousSolutionAlreadyReached, false
 }
 
-func getNextBracketsTree(input string) (string, error) {
-	tail, size, err := StringToBracketsSteps(input)
+// GetNextBracketsTree generates next brackets tree sequence based on previous one from input
+func GetNextBracketsTree(input string) (string, error) {
+
+	tail, size, err := stringToBracketsSteps(input)
 	if err != nil {
 		return "", err
 	}
@@ -225,14 +108,18 @@ func getNextBracketsTree(input string) (string, error) {
 	nextTail, _, solutionFound := getNextStepsTail(tail, []BracketStep{}, size, size, false)
 
 	if !solutionFound {
-		return fmt.Sprintf("%v -> %v", tail, "next solution not found"), nil
+		output := ""
+		for i := 0; i < size+1; i++ {
+			output += "()"
+		}
+		return output, nil
 	}
 
-	return fmt.Sprintf("%v -> %v", tail, nextTail), nil
+	return bracketsStepsToString(nextTail), nil
 }
 
 // BracketsStepsToString represent bracket steps as string
-func BracketsStepsToString(tail []BracketStep) string {
+func bracketsStepsToString(tail []BracketStep) string {
 	output := ""
 	for _, step := range tail {
 		for i := 0; i < step.x; i++ {
@@ -245,26 +132,4 @@ func BracketsStepsToString(tail []BracketStep) string {
 	return output
 }
 
-// NextBracket recursively generates bracket ledder (each element is one ledder step with width and height)
-func NextBracket(tail []BracketStep, sizeX int, sizeY int, output func([]BracketStep)) {
-	//fmt.Println(fmt.Sprintf("next bracket"))
-
-	if sizeX == 0 && sizeY == 0 {
-		output(tail)
-		return
-	}
-
-	for i := 1; i <= sizeX; i++ {
-		for j := 1; j <= sizeY-(sizeX-i); j++ {
-
-			//fmt.Println(fmt.Sprintf("%v,%v, %v,%v", i, j, size_x, size_y))
-			nextTail := append(tail, BracketStep{x: i, y: j})
-
-			NextBracket(nextTail, sizeX-i, sizeY-j, output)
-		}
-	}
-}
-
-func main() {
-	NextBracket([]BracketStep{}, 4, 4, printBrackets)
-}
+func main() {}
--- Catalan_test.go
+++ Catalan_test.go
@@ -1,9 +1,112 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
-func Test_Brackets(t *testing.T) {
-	NextBracket([]BracketStep{}, 6, 6, printBrackets)
+func nextBracket(tail []BracketStep, sizeX int, sizeY int, output *[]string) {
+
+	if sizeX == 0 && sizeY == 0 {
+		*output = append(*output, bracketsStepsToString(tail))
+		return
+	}
+
+	for i := 1; i <= sizeX; i++ {
+		for j := 1; j <= sizeY-(sizeX-i); j++ {
+			nextTail := append(tail, BracketStep{x: i, y: j})
+			nextBracket(nextTail, sizeX-i, sizeY-j, output)
+		}
+	}
+}
+
+func Test_Sequence(t *testing.T) {
+	solutions1 := []string{}
+	solutions2 := []string{}
+
+	nextBracket([]BracketStep{}, 8, 8, &solutions1)
+
+	currentSequence := solutions1[0]
+	for i := 0; i < len(solutions1); i++ {
+		next, err := GetNextBracketsTree(currentSequence)
+		if err != nil {
+			t.Errorf("getNextBracketsTree('%v') returned error:%v", currentSequence, err)
+		}
+		solutions2 = append(solutions2, currentSequence)
+		currentSequence = next
+	}
+
+	for i, solution := range solutions1 {
+		t.Log(fmt.Sprintf("%v  %v %v", i+1, solution, solutions2[i]))
+		if solution != solutions2[i] {
+			t.Errorf("sequences does not match")
+		}
+	}
+}
+
+func testSequenceError(t *testing.T, testSequence string) {
+	_, err := GetNextBracketsTree(testSequence)
+
+	if err == nil {
+		t.Errorf("error for '%v' sequence does not catched", testSequence)
+	} else {
+		t.Log(fmt.Sprintf("error for '%v' successfully catched.\nerror description:%v", testSequence, err))
+	}
+}
+
+func testSequenceSuccess(t *testing.T, testSequence string, expectingAnswer string) {
+	answer, err := GetNextBracketsTree(testSequence)
+
+	if err != nil {
+		t.Errorf("error for %v :%v", testSequence, err)
+	}
+
+	if answer != expectingAnswer {
+		t.Errorf("expecting answer %v for %v are not equal to returned answer %v", expectingAnswer, testSequence, answer)
+	}
+
+}
+
+func Test_Empty(t *testing.T) {
+	testSequenceError(t, "")
+}
+
+func Test_IncorrectSymbolFromStart(t *testing.T) {
+	testSequenceError(t, "a(()(()))")
+}
+
+func Test_IncorrectSymbolAtTheEnd(t *testing.T) {
+	testSequenceError(t, "(()(()))b")
+}
+
+func Test_IncorrectSymbolInTheMiddle(t *testing.T) {
+	testSequenceError(t, "(()((c)))")
+}
+
+func Test_LostOpeningBracket(t *testing.T) {
+	testSequenceError(t, "())")
+}
+
+func Test_LostClosingBracket(t *testing.T) {
+	testSequenceError(t, "(()")
+}
+
+func Test_WrongBracketsSequence(t *testing.T) {
+	testSequenceError(t, "())(")
+}
+
+func Test_FirstBracket(t *testing.T) {
+	testSequenceSuccess(t, "()", "()()")
+}
+
+func Test_FirstNBrackets(t *testing.T) {
+	currentSequence := "()"
+	for i := 0; i < 120; i++ {
+		next, err := GetNextBracketsTree(currentSequence)
+		if err != nil {
+			t.Errorf("getNextBracketsTree('%v') returned error:%v", currentSequence, err)
+		}
+		t.Log(fmt.Sprintf("%v %v -> %v", i, currentSequence, next))
+		currentSequence = next
+	}
 }
