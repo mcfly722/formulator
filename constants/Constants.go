13a54b1f1675cb87a78e66122866643d4cdd1e2b
mcfly722 <7221798@gmail.com>
2021-09-26 19:19:32 +0300

    constant recombination works

--- constants/Constants.go
+++ constants/Constants.go
@@ -1,6 +1,72 @@
 package constants
 
+const constantIterationIndex = 0
+const constantPreviousIterationValue = 1
+const constantArgument = 2
+
+func nextCombinationDigit(constants *[]float64,
+	combination []float64,
+	depth int,
+	maxIterationIndexes int,
+	maxPreviousIterationValue int,
+	maxArguments int,
+	ready func(constantsCombination []float64)) {
+
+	if len(combination) < depth {
+		for i := 0; i < len(*constants); i++ {
+
+			v := (*constants)[i]
+
+			currentIterationIndexes := maxIterationIndexes
+			currentPreviousIterationValue := maxPreviousIterationValue
+			currentArguments := maxArguments
+
+			if v == constantIterationIndex {
+				currentIterationIndexes--
+			}
+
+			if v == constantPreviousIterationValue {
+				currentPreviousIterationValue--
+			}
+
+			if v == constantArgument {
+				currentArguments--
+			}
+
+			if currentIterationIndexes >= 0 && currentPreviousIterationValue >= 0 && currentArguments >= 0 {
+				nextCombination := append(combination, v)
+				nextCombinationDigit(
+					constants,
+					nextCombination,
+					depth,
+					currentIterationIndexes,
+					currentPreviousIterationValue,
+					currentArguments,
+					ready)
+			}
+		}
+	} else {
+		ready(combination)
+	}
+}
+
 // Recombination recombine all constants and call ready function for each combination
-func Recombination(constants *[]float64, ready func(constantsCombination *[]float64)) {
-	ready(constants)
+func Recombination(
+	constants *[]float64,
+	digits int,
+	maxIterationIndexes int,
+	maxPreviousIterationValue int,
+	maxArguments int,
+	ready func(constantsCombination []float64)) {
+
+	combination := []float64{}
+	nextCombinationDigit(
+		constants,
+		combination,
+		digits,
+		maxIterationIndexes,
+		maxPreviousIterationValue,
+		maxArguments,
+		ready)
+
 }
--- constants/Constants_test.go
+++ constants/Constants_test.go
@@ -2,19 +2,20 @@ package constants
 
 import (
 	"fmt"
-	"math"
 	"testing"
 )
 
 // go test -v constants.go constants_test.go
 
 func Test_ConstantsCombinations(t *testing.T) {
+	i := 1
 
-	constants := []float64{2, 3, 5, -1, math.Pi}
+	constants := []float64{constantIterationIndex, constantPreviousIterationValue, constantArgument}
 
-	ready := func(constantsCombination *[]float64) {
-		t.Log(fmt.Sprintf("%v", constantsCombination))
+	ready := func(constantsCombination []float64) {
+		t.Log(fmt.Sprintf("%5v %v", i, constantsCombination))
+		i++
 	}
 
-	Recombination(&constants, ready)
+	Recombination(&constants, 3, 1, 2, 3, ready)
 }
