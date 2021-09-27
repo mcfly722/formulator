bcbc70f1e81395e464877630c691dd34ca0433c0
mcfly722 <7221798@gmail.com>
2021-09-28 00:57:33 +0300

    no absolute power

--- operators/Operators_test.go
+++ operators/Operators_test.go
@@ -27,17 +27,17 @@ func Test_Add(t *testing.T) {
 
 }
 
-func Test_AbsPower(t *testing.T) {
+func Test_Power(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
-	a := rand.Float64() * -10 // positive only numbers
+	a := rand.Float64() * 10 // positive only numbers :)))
 	b := rand.Float64()*4 - 2
-	o := newOperator(func(a float64, b float64) float64 { return math.Pow(math.Abs(a), b) }, "^")
+	o := newOperator(func(a float64, b float64) float64 { return math.Pow(a, b) }, "^")
 
 	c := o.Calculate(a, b)
 
 	expression := fmt.Sprintf("%v = %v", o.OperatorExpressionToString(fmt.Sprintf("%f", a), fmt.Sprintf("%f", b)), c)
 
-	if c != math.Pow(math.Abs(a), b) {
+	if c != math.Pow(a, b) {
 		t.Errorf(expression)
 	} else {
 		t.Log(expression)
@@ -49,7 +49,7 @@ func Test_OperatorsRecombination(t *testing.T) {
 
 	addOperator := newOperator(func(a float64, b float64) float64 { return a + b }, "+")
 	multiplyOperator := newOperator(func(a float64, b float64) float64 { return a * b }, "*")
-	powerOperator := newOperator(func(a float64, b float64) float64 { return math.Pow(math.Abs(a), b) }, "^")
+	powerOperator := newOperator(func(a float64, b float64) float64 { return math.Pow(a, b) }, "^")
 
 	availableOperatorsTypes := []*Operator{addOperator, multiplyOperator, powerOperator}
 
